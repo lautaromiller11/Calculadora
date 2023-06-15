c3541d2a455a630b718b0b415d21fb749600fa6d
lautaro miller <lautaromiller11@gmail.com>
2023-06-15 20:08:38 -0300

    subida proyecto calculadora a repositorio

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".botones");
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonPresionado = boton.textContent;
+
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if (boton.id === "borrar") {
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+            return;
+        }
+
+        if (boton.id === "igual") {
+            pantalla.textContent = eval(pantalla.textContent);
+            return;
+        }
+
+        if (pantalla.textContent === "0") {
+            pantalla.textContent = botonPresionado;
+        } else {
+            pantalla.textContent += botonPresionado;
+        }
+    });
+})
